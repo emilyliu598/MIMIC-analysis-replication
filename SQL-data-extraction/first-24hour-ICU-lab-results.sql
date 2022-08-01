03d10d8fb780e78de346647237ae9f34b08a8253
emilyliu598 <105017987+emilyliu598@users.noreply.github.com>
2022-08-01 08:02:09 -0400

    Create first-24hour-ICU-lab-results.sql

---
+++ SQL-data-extraction/first-24hour-ICU-lab-results.sql
@@ -0,0 +1,13 @@
+WITH t1 AS ( 
+  --- laboratory results table (SAPS-II: WBC, Sodium, Potassium, BUN, BIC. SOFA: Creatinine, Platelets)
+  SELECT c.*, fdl.wbc_max AS wbc_icu, 
+  fdl.sodium_min AS sodium_icu, fdl.potassium_max AS potassium_icu, 
+  fdl.bun_max AS bun_icu, fdl.bicarbonate_min AS bicarbonate_icu, 
+  fdl.creatinine_max AS creatinine_icu, 
+  fdl.platelets_min AS platelets_icu
+  FROM `physionet-data.mimic_derived.first_day_lab` fdl
+  INNER JOIN `emilyliu-chl7001h.cohort1.cohort` c
+  ON fdl.subject_id = c.subject_id AND fdl.stay_id = c.stay_id
+)
+SELECT *
+FROM t1
